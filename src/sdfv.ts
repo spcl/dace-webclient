8941e1113f2f92fa62f9e102b90e8a778d37c0f7
Philipp Schaad <philipp.schaad@inf.ethz.ch>
2024-05-08 15:58:36 +0200

    Formatting and a fix to how ranges are printed (#144)

--- src/sdfv.ts
+++ src/sdfv.ts
@@ -62,8 +62,9 @@ export class SDFV {
     public static TEXT_LOD: number = 1.5; // 1.5
 
     // Pixel threshold for not drawing State and NestedSDFG contents.
-    // This threshold behaves differently than the ones above. The State's size is compared to this 
-    // threshold and if the State is smaller its contents are not drawn in the renderer.
+    // This threshold behaves differently than the ones above. The State's size
+    // is compared to this threshold and if the State is smaller its contents
+    // are not drawn in the renderer.
     public static STATE_LOD: number = 100; // 100
 
     public static DEFAULT_CANVAS_FONTSIZE: number = 10;
