d0ba2c57ec15ff338553d5282d0fb79b8b258d8e
Philipp Schaad <schaad.phil@gmail.com>
2025-06-16 17:04:52 +0200

    Update

--- src/utils/bounding_box.ts
+++ src/utils/bounding_box.ts
@@ -1,4 +1,4 @@
-// Copyright 2019-2024 ETH Zurich and the DaCe authors. All rights reserved.
+// Copyright 2019-2025 ETH Zurich and the DaCe authors. All rights reserved.
 
 import type { GraphEdge } from 'dagre';
 import type { Edge } from '../renderer/sdfg/sdfg_elements';
