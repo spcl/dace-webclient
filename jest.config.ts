89898b553544902b9f4bbd5d81c2225902e209a8
Philipp Schaad <schaad.phil@gmail.com>
2025-07-16 08:57:08 +0200

    Fix attempt for tests

--- jest.config.ts
+++ jest.config.ts
@@ -194,7 +194,10 @@ const config: Config = {
     // testRunner: 'jest-circus/runner',
 
     // A map from regular expressions to paths to transformers
-    // transform: undefined,
+    transform: {
+        '^.+\\.(ts|tsx)?$': 'ts-jest',
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    },
 
     // An array of regexp pattern strings that are matched against all source
     // file paths, matched files will skip transformation
