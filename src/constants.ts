f353ae6fef3b9200e9e4b4d48caa00ea5dbf4715
Philipp Schaad <schaad.phil@gmail.com>
2025-10-30 14:21:28 +0100

    Fix constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 // Copyright 2019-2025 ETH Zurich and the DaCe authors. All rights reserved.
 
-export const SDFV_LINE_HEIGHT: number = 20;
+export const SDFV_LINE_HEIGHT: number = 10;
 export const SDFV_LABEL_MARGIN_H: number = 5;
-export const SDFV_LABEL_MARGIN_V: number = 3;
+export const SDFV_LABEL_MARGIN_V: number = 1;
 // Points-per-pixel threshold for not drawing Arrowheads of edges.
 export const SDFV_ARROW_LOD: number = 2.0;
 // Points-per-pixel threshold for not drawing connectors.
