9aecc74a7f7449ac8a1caf37f2c06228252662aa
Philipp Schaad <schaad.phil@gmail.com>
2025-12-04 09:32:35 +0100

    Fix missing transpilation in JEST

--- jest.config.ts
+++ jest.config.ts
@@ -201,10 +201,10 @@ const config: Config = {
 
     // An array of regexp pattern strings that are matched against all source
     // file paths, matched files will skip transformation
-    // transformIgnorePatterns: [
-    //   '\\\\node_modules\\\\',
-    //   '\\.pnp\\.[^\\\\]+$'
-    // ],
+    transformIgnorePatterns: [
+        //'\\\\node_modules\\\\',
+        'node_modules/(?!(rendure)/)',
+    ],
 
     // An array of regexp pattern strings that are matched against all modules
     // before the module loader will automatically return a mock for them
